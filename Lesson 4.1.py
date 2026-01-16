70ef4bccd118cc882076482129f9c10a1fd096ee
SCALex050 <scalex050@gmail.com>
2026-01-16 23:59:40 +0300

    Add files via upload

---
+++ Lesson 4.1.py
@@ -0,0 +1,12 @@
+print ("Введите длину прямоугольника в мм")
+lenght = float(input())
+
+print ("Введите ширину прямоугольника в мм")
+width = float(input())
+ 
+
+area = width * lenght
+perimeter = 2 * (width + lenght)
+
+print ("Площадь прямоугольника равна -", area, "мм².")
+print ("Периметр прямоугольника равен -", perimeter,"мм." )
