ea8814553b4d796bc9cb267ec2dccc4efe85b0dc
SCALex050 <scalex050@gmail.com>
2026-01-18 17:42:39 +0300

    Update Lesson 8.3.py

--- Lesson 8.3.py
+++ Lesson 8.3.py
@@ -7,4 +7,18 @@ b = 0
 
 for i in range (n):
     w.append (int(input("Введите вес рыбака - ")))
+w.sort(reverse = True)
 
+while len(w) > 0:
+    if len(w) >= 2:
+        if (w[0] + w[-1]) <= m:
+            b += 1
+            w.pop()
+            w.pop(0)
+        else:
+            b += 1
+            w.pop(0)
+    else:
+        b += 1 
+        w.pop(0)
+print (f"Количество лодок, чтобы перевести рыбаков - {b}")
