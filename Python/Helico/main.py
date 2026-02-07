53cb863c3a0cfdc7b29755f790b8626a10389568
SCALex050 <scalex050@gmail.com>
2026-02-07 22:40:07 +0300

    Update main.py

--- Python/Helico/main.py
+++ Python/Helico/main.py
@@ -1,1 +1,32 @@
 
+from map import Map
+import time 
+import os 
+
+TICK_SLEEP = 0.05
+TREE_UPDATE = 50
+FIRE_UPDATE = 25
+
+tmp = Map(20, 10)
+tmp.generate_forest (1, 10)
+tmp.generate_river (10)
+tmp.generate_river (10)
+tmp.generate_river (10)
+tmp.add_fire()
+tmp.add_fire()
+tmp.add_fire()
+tmp.add_fire()
+tmp.add_fire()
+tmp.add_fire()
+tmp.print_map()
+
+tick = 1 
+
+while True:
+    os.system("cls")
+    print ("Tick", tick)
+    tmp.print_map()
+    tick +=1 
+    time.sleep(TICK_SLEEP)
+    if (tick % TREE_UPDATE == 0):
+	tmp.generate_tree()
